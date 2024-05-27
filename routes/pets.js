af3321f861bec98b8cedf28be027d8633b9891e7
Louisa Goncharenko <louisa.gonch@gmail.com>
2024-05-27 16:56:33 -0400

    Implemented search pagination

--- routes/pets.js
+++ routes/pets.js
@@ -58,16 +58,17 @@ module.exports = (app) => {
   // SEARCH PET
   app.get('/search', (req, res) => {
     const term = new RegExp(req.query.term, 'i');
-    Pet.find({$or:[
-      {'name': term},
-      {'species': term}
-    ]}).exec((err, pets) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send("Error searching pets");
-      }
-      res.render('pets-index', { pets: pets });
-    });
+    const page = req.query.page || 1
+    Pet.paginate(
+      {
+        $or: [
+          { 'name': term },
+          { 'species': term }
+        ]
+      },
+      { page: page }).then((results) => {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });
+      });
   });
 
 
